dd4108492beebf996792efd53e2e7d10ace10a40
Arseniy Dmitriev <44650732+AresDm0005@users.noreply.github.com>
2020-04-01 21:10:39 +0500

    task1 solved

---
+++ Task1/Task1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Нам гарантирован корректный ввод -> проверок не нужно
+            string[] input = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            int a = int.Parse(input[0]);
+            int b = int.Parse(input[1]);
+            int c = int.Parse(input[2]);
+            int r = int.Parse(Console.ReadLine());
+
+            // Полупериметр
+            double p = (a + b + c) / 2.0;
+            // Радиус вписаной окружности (максимально возможный)
+            double Rmax = Math.Sqrt((p - a) * (p - b) * (p - c) / p);
+
+            if (r <= Rmax) Console.WriteLine("YES");
+            else Console.WriteLine("NO");
+        }
+    }
+}
