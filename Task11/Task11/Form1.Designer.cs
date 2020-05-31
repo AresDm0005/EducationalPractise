79e7b0313d1355a69d5a20e1b2c028064767ab7e
Arseniy Dmitriev <44650732+AresDm0005@users.noreply.github.com>
2020-05-31 21:27:53 +0500

    Task11

---
+++ Task11/Task11/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace Task11
+{
+    partial class Task11
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.taskLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.inputBox = new System.Windows.Forms.TextBox();
+            this.outputBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.goButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.rndButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // taskLabel
+            // 
+            this.taskLabel.AutoSize = true;
+            this.taskLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.taskLabel.Location = new System.Drawing.Point(12, 9);
+            this.taskLabel.Name = "taskLabel";
+            this.taskLabel.Size = new System.Drawing.Size(659, 18);
+            this.taskLabel.TabIndex = 0;
+            this.taskLabel.Text = "\tЗадание 11. Программа расшифровки сообщения, которое кодируется так: 0 -> 000; 1" +
+    " -> 111";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(285, 18);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Поступившее сообщение (с ошибками):";
+            // 
+            // inputBox
+            // 
+            this.inputBox.Location = new System.Drawing.Point(15, 63);
+            this.inputBox.Name = "inputBox";
+            this.inputBox.Size = new System.Drawing.Size(678, 20);
+            this.inputBox.TabIndex = 2;
+            this.inputBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.inputBox_KeyPress);
+            this.inputBox.Validating += new System.ComponentModel.CancelEventHandler(this.inputBox_Validating);
+            this.inputBox.Validated += new System.EventHandler(this.inputBox_Validated);
+            // 
+            // outputBox
+            // 
+            this.outputBox.Location = new System.Drawing.Point(15, 130);
+            this.outputBox.Name = "outputBox";
+            this.outputBox.ReadOnly = true;
+            this.outputBox.Size = new System.Drawing.Size(678, 20);
+            this.outputBox.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(12, 109);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(218, 18);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Расшифрованное сообщение:";
+            // 
+            // goButton
+            // 
+            this.goButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.goButton.Location = new System.Drawing.Point(712, 80);
+            this.goButton.Name = "goButton";
+            this.goButton.Size = new System.Drawing.Size(118, 25);
+            this.goButton.TabIndex = 5;
+            this.goButton.Text = "Расшифровать!";
+            this.goButton.UseVisualStyleBackColor = true;
+            this.goButton.Click += new System.EventHandler(this.goButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.exitButton.Location = new System.Drawing.Point(712, 127);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(118, 25);
+            this.exitButton.TabIndex = 6;
+            this.exitButton.Text = "Закрыть";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // rndButton
+            // 
+            this.rndButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.rndButton.Location = new System.Drawing.Point(712, 49);
+            this.rndButton.Name = "rndButton";
+            this.rndButton.Size = new System.Drawing.Size(118, 25);
+            this.rndButton.TabIndex = 7;
+            this.rndButton.Text = "Сгенерировать";
+            this.rndButton.UseVisualStyleBackColor = true;
+            this.rndButton.Click += new System.EventHandler(this.rndButton_Click);
+            // 
+            // Task11
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.ClientSize = new System.Drawing.Size(843, 163);
+            this.ControlBox = false;
+            this.Controls.Add(this.rndButton);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.goButton);
+            this.Controls.Add(this.outputBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.inputBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.taskLabel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Task11";
+            this.Text = "Задание 11";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label taskLabel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox inputBox;
+        private System.Windows.Forms.TextBox outputBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button goButton;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.Button rndButton;
+    }
+}
+
---
+++ Task11/Task11/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.ComponentModel;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace Task11
+{
+    public partial class Task11 : Form
+    {
+        private ErrorProvider incorrectInput;
+        private Random rnd;
+
+        public Task11()
+        {
+            InitializeComponent();
+            incorrectInput = new ErrorProvider();
+            incorrectInput.BlinkStyle = ErrorBlinkStyle.NeverBlink;
+            rnd = new Random();
+        }
+
+        private string Solve(string inp)
+        {
+            string txt = "";
+            for (int i = 0; i < inp.Length / 3; i++)
+                if (inp[i * 3] == inp[i * 3 + 1] || inp[i * 3] == inp[i * 3 + 2])
+                    txt += inp[i * 3];
+                else
+                    txt += inp[i * 3 + 1];
+
+            return txt;
+        }
+
+        private void rndButton_Click(object sender, EventArgs e)
+        {
+            string txt = "";
+            int length = rnd.Next(3, 15) * 3;
+
+            for (int i = 0; i < length; i++)
+                txt += rnd.Next(0, 2);
+
+            inputBox.Text = txt;
+        }
+
+        private void goButton_Click(object sender, EventArgs e)
+        {
+            outputBox.Text = Solve(inputBox.Text);
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void inputBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!Regex.IsMatch(e.KeyChar.ToString(), @"[01]") && e.KeyChar != 8)            
+                e.Handled = true;            
+        }
+
+        private void inputBox_Validating(object sender, CancelEventArgs e)
+        {
+            int length = ((TextBox)sender).Text.Length;
+            if(length % 3 != 0)
+            {
+                e.Cancel = true;
+                inputBox.SelectAll();
+                incorrectInput.SetError((Control)sender, $"Длина входящего сообщения должна быть кратна 3\nТекущая длина: {length}");
+            }
+        }
+
+        private void inputBox_Validated(object sender, EventArgs e)
+        {
+            incorrectInput.SetError((Control)sender, String.Empty);
+        }
+    }
+}
