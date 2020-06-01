6e6873c2de4c5fdd7535a4252fbcf72d0fa15294
Arseniy Dmitriev <44650732+AresDm0005@users.noreply.github.com>
2020-06-01 23:35:58 +0500

    Task6 add

---
+++ Task6/Task6/Task6Form.Designer.cs
@@ -0,0 +1,254 @@
+﻿namespace Task6
+{
+    partial class Task6Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Task6Form));
+            this.taskLabel = new System.Windows.Forms.Label();
+            this.a1Box = new System.Windows.Forms.TextBox();
+            this.a1 = new System.Windows.Forms.Label();
+            this.a2 = new System.Windows.Forms.Label();
+            this.a2Box = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.a3Box = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.mBox = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.nBox = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.resultLabel = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.resultBox = new System.Windows.Forms.TextBox();
+            this.goButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // taskLabel
+            // 
+            this.taskLabel.AutoSize = true;
+            this.taskLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.taskLabel.Location = new System.Drawing.Point(12, 9);
+            this.taskLabel.Name = "taskLabel";
+            this.taskLabel.Size = new System.Drawing.Size(820, 54);
+            this.taskLabel.TabIndex = 0;
+            this.taskLabel.Text = resources.GetString("taskLabel.Text");
+            // 
+            // a1Box
+            // 
+            this.a1Box.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.a1Box.Location = new System.Drawing.Point(30, 100);
+            this.a1Box.Name = "a1Box";
+            this.a1Box.Size = new System.Drawing.Size(60, 24);
+            this.a1Box.TabIndex = 1;
+            this.a1Box.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Seq_KeyPress);
+            // 
+            // a1
+            // 
+            this.a1.AutoSize = true;
+            this.a1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.a1.Location = new System.Drawing.Point(50, 80);
+            this.a1.Name = "a1";
+            this.a1.Size = new System.Drawing.Size(24, 18);
+            this.a1.TabIndex = 2;
+            this.a1.Text = "a1";
+            // 
+            // a2
+            // 
+            this.a2.AutoSize = true;
+            this.a2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.a2.Location = new System.Drawing.Point(140, 80);
+            this.a2.Name = "a2";
+            this.a2.Size = new System.Drawing.Size(24, 18);
+            this.a2.TabIndex = 4;
+            this.a2.Text = "a2";
+            // 
+            // a2Box
+            // 
+            this.a2Box.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.a2Box.Location = new System.Drawing.Point(120, 100);
+            this.a2Box.Name = "a2Box";
+            this.a2Box.Size = new System.Drawing.Size(60, 24);
+            this.a2Box.TabIndex = 2;
+            this.a2Box.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Seq_KeyPress);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(230, 80);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(24, 18);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "a3";
+            // 
+            // a3Box
+            // 
+            this.a3Box.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.a3Box.Location = new System.Drawing.Point(210, 100);
+            this.a3Box.Name = "a3Box";
+            this.a3Box.Size = new System.Drawing.Size(60, 24);
+            this.a3Box.TabIndex = 5;
+            this.a3Box.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Seq_KeyPress);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(320, 80);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(21, 18);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "M";
+            // 
+            // mBox
+            // 
+            this.mBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.mBox.Location = new System.Drawing.Point(300, 100);
+            this.mBox.Name = "mBox";
+            this.mBox.Size = new System.Drawing.Size(60, 24);
+            this.mBox.TabIndex = 7;
+            this.mBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Params_KeyPress);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(410, 80);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(19, 18);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "N";
+            // 
+            // nBox
+            // 
+            this.nBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.nBox.Location = new System.Drawing.Point(390, 100);
+            this.nBox.Name = "nBox";
+            this.nBox.Size = new System.Drawing.Size(60, 24);
+            this.nBox.TabIndex = 9;
+            this.nBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Params_KeyPress);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(12, 141);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(164, 18);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Выполнилось раньше:";
+            // 
+            // resultLabel
+            // 
+            this.resultLabel.AutoSize = true;
+            this.resultLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.resultLabel.Location = new System.Drawing.Point(196, 141);
+            this.resultLabel.Name = "resultLabel";
+            this.resultLabel.Size = new System.Drawing.Size(16, 18);
+            this.resultLabel.TabIndex = 12;
+            this.resultLabel.Text = "1";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label7.Location = new System.Drawing.Point(12, 172);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(576, 18);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Элементы последовательности, в соответствии с тем что выполнилось раньше:";
+            // 
+            // resultBox
+            // 
+            this.resultBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.resultBox.Location = new System.Drawing.Point(15, 193);
+            this.resultBox.Name = "resultBox";
+            this.resultBox.Size = new System.Drawing.Size(814, 24);
+            this.resultBox.TabIndex = 14;
+            // 
+            // goButton
+            // 
+            this.goButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.goButton.Location = new System.Drawing.Point(496, 98);
+            this.goButton.Name = "goButton";
+            this.goButton.Size = new System.Drawing.Size(116, 28);
+            this.goButton.TabIndex = 15;
+            this.goButton.Text = "Построить!";
+            this.goButton.UseVisualStyleBackColor = true;
+            this.goButton.Click += new System.EventHandler(this.goButton_Click);
+            // 
+            // Task6Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.ClientSize = new System.Drawing.Size(841, 229);
+            this.Controls.Add(this.goButton);
+            this.Controls.Add(this.resultBox);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.resultLabel);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.nBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.mBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.a3Box);
+            this.Controls.Add(this.a2);
+            this.Controls.Add(this.a2Box);
+            this.Controls.Add(this.a1);
+            this.Controls.Add(this.a1Box);
+            this.Controls.Add(this.taskLabel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Task6Form";
+            this.Text = "Задание 6";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label taskLabel;
+        private System.Windows.Forms.TextBox a1Box;
+        private System.Windows.Forms.Label a1;
+        private System.Windows.Forms.Label a2;
+        private System.Windows.Forms.TextBox a2Box;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox a3Box;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox mBox;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox nBox;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label resultLabel;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox resultBox;
+        private System.Windows.Forms.Button goButton;
+    }
+}
+
---
+++ Task6/Task6/Task6Form.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace Task6
+{
+    public partial class Task6Form : Form
+    {
+        private int N, M, tmp;
+        private int[] arr;     
+
+        public Task6Form()
+        {
+            InitializeComponent();
+        }
+
+        private void Seq_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!Regex.IsMatch(e.KeyChar.ToString(), @"[\d\-]") && e.KeyChar != 8)
+                e.Handled = true;
+        }
+
+        private void Params_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!Regex.IsMatch(e.KeyChar.ToString(), @"[\d]") && e.KeyChar != 8)
+                e.Handled = true;
+        }
+
+
+        private bool HandleConst()
+        {
+            bool ok = false;
+            string error = "";
+
+            ok = int.TryParse(nBox.Text, out N);
+            if (!ok)
+                error += "Значение N введено некорректно или не введено совсем\n";
+
+            if (ok && N < 4)
+                error += "Значение N должно быть больше 3";
+                
+
+            ok = int.TryParse(mBox.Text, out M);
+            if (!ok)
+                error += "Значение M введено некорректно или не введено совсем\n";
+
+            if (error != "")
+                MessageBox.Show(error);
+
+            return error == "";
+        }
+
+        private bool HandleInputs()
+        {
+            bool ok = HandleConst();            
+            if (!ok) return ok;
+            string error = "";
+
+            arr = new int[N];
+            tmp = M;
+
+            for(int i = 0; i < 3; i++)
+            {
+                ok = int.TryParse(((TextBox)Controls.Find($"a{i + 1}Box", true)[0]).Text, out arr[i]);
+                N--;
+
+                if (arr[i] % 3 == 0)
+                    M--;
+
+                if (!ok)
+                    error += $"Значение a{i + 1} введено некорректно или не введено совсем\n";
+            }
+
+            if (error != "")
+                MessageBox.Show(error, "", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+            return error == "";
+        }
+
+        private int Recursion(int index)
+        {
+            if (N <= 0 && M <= 0) return 3;
+            else if (N <= 0) return 1;
+            else if (M <= 0) return 2;
+
+            arr[index] = 2 * Math.Abs(arr[index - 1] - arr[index - 2]) + arr[index - 3];
+            N--;
+            if (arr[index] % 3 == 0) M--;
+            return Recursion(index+1);            
+        }
+
+        private void goButton_Click(object sender, EventArgs e)
+        {   
+            if (!HandleInputs())
+                return;
+            
+            // 1 - по N, 2 - по M, 3 - и по N, и по M
+            int result = Recursion(3);
+            string txt = "";
+
+            if(result == 1)
+            {
+                for (int i = 0; i < arr.Length - 1; i++)
+                    txt += $"{arr[i]}, ";
+
+                txt += arr[arr.Length - 1].ToString();
+
+                resultLabel.Text = "построено N элементов последовательности";
+                resultBox.Text = txt;                
+            } 
+            else if (result == 2)
+            {
+
+                foreach (int it in arr)
+                    if (it % 3 == 0 && tmp-- > 0)
+                        if (tmp != 0)
+                            txt += $"{it}, ";
+                        else 
+                            txt += $"{it}";                    
+
+                resultLabel.Text = "найдено M элементов кратных 3 последовательности";
+                resultBox.Text = txt;
+            }
+            else
+            {
+                foreach (int it in arr)
+                    txt += $"{it}, ";
+
+                resultLabel.Text = "одновременно было построено N элементов и найдено M кратных 3";
+                resultBox.Text = txt;
+            }
+        }
+    }
+}
